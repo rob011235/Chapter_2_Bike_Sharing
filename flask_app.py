2ab67810eeeb449f31eca24a02f4720e6f82d0ff
Robert Garner <rgarner011235@gmail.com>
2025-09-24 16:14:51 -0600

    first commit

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+from bike_calc import calc_bike_rentals
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/calculate", methods=['POST'])
+def calculate():
+    holiday = request.form.get("isHoliday") == "on"
+    season = int(request.form.get("season"))
+    temp = float(request.form.get("temp"))
+    bike_rentals = calc_bike_rentals(holiday, season, temp)
+    return render_template("results.html", rentals=bike_rentals)
